7f51be60394da79d6de073ed4a1afe23032d18ed
JIN <gustmd9696@naver.com>
2022-10-25 11:23:37 +0900

    Step03_Class 프로젝트 추가함

---
+++ Step03_Class/src/test/main/MainClass02.java
@@ -0,0 +1,13 @@
+package test.main;
+
+import test.mypac.MyUtil;
+
+public class MainClass02 {
+	public static void main(String[] args) {
+		// static 메소드는 클래스명에 . 찍어서 바로 호출 할수있다.
+		MyUtil.send();
+		// static 필드는 클래스명에 . 찍어서 바로 참조 할수있다.
+		MyUtil.version="1.0";
+	}
+
+}
---
+++ Step03_Class/src/test/mypac/MemBer.java
@@ -0,0 +1,17 @@
+package test.mypac;
+
+public class MemBer {
+	//번호를 저장할 필드
+	public int num;
+	//이름을 저장할 필드
+	public String name;
+	//주소를 저장할 필드
+	public String addr;
+	
+	//메소드
+	public void showInfo() {
+		System.out.println( "번호:"+this.num+", 이름:"+this.name+", 주소:"+this.addr);
+	}
+	
+
+}
