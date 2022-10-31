0090beb62414baaa20f86db17e01db8966321993
JIN <gustmd9696@naver.com>
2022-10-31 12:23:14 +0900

    Step10 수정 및 추가

--- Step09_AbstractClass/src/test/main/MainClass05.java
+++ Step09_AbstractClass/src/test/main/MainClass05.java
@@ -28,6 +28,7 @@ public class MainClass05 {
 	
 	
 	public static void main(String[] args) {
+		//메소드 호출하면서 static 필드에 미리 준비된 값을 전달하기
 		useWeapon(w1);
 		// Anonymous Local Inner Class 를 이용해서 Weapon type 의 참조값 얻어내기
 		Weapon w2=new Weapon() { 
@@ -37,9 +38,10 @@ public class MainClass05 {
 			}
 			
 		};
+		//메소드 호추하면서 지역변수에 미리 준비된 값을 전달하기
 		useWeapon(w2);
 	}
-
+	//메소드 호출하면서 값을 즉석에서 만들어 잔달하기
 	public static void useWeapon(Weapon w) {
 		w.attack();
 		w.prepare();
